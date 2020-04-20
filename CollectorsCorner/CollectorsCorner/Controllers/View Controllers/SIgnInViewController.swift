d168be21bcfd496d6fdc03a43b8f5e34f406ea40
Colby Harris <griff33@gmail.com>
2020-04-20 09:02:00 -0600

    Created SignIn view controller and started on storyBoard. Added Sign in with apple button and implementation

---
+++ CollectorsCorner/CollectorsCorner/Controllers/View Controllers/SIgnInViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SIgnInViewController.swift
+//  CollectorsCorner
+//
+//  Created by Colby Harris on 4/19/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class ASAuthorizationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func setUpSignInAppleButton() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAppleIdRequest), for: .touchUpInside)
+        authorizationButton.cornerRadius = 10
+        //Add button on some view or stack
+        self.signInButtonStack.addArrangedSubview(authorizationButton)
+    }
+    
+    @objc func handleAppleIdRequest() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ CollectorsCorner/CollectorsCorner/Controllers/View Controllers/SignInController.swift
@@ -0,0 +1,65 @@
+//
+//  SIgnInViewController.swift
+//  CollectorsCorner
+//
+//  Created by Colby Harris on 4/19/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class SignInViewController: UIViewController, ASAuthorizationControllerDelegate {
+    
+    //MARK: - Properties and Outlets
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginProviderStackView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSignInAppleButton()
+    }
+    
+    @IBAction func logInButtonTapped(_ sender: Any) {
+    }
+    
+    func setUpSignInAppleButton() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAppleIdRequest), for: .touchUpInside)
+        authorizationButton.cornerRadius = 10
+        //Add button on some view or stack
+        self.loginProviderStackView.addArrangedSubview(authorizationButton)
+    }
+    
+    @objc func handleAppleIdRequest() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as?  ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            print("User id is \(userIdentifier) \n Full Name is \(String(describing: fullName)) \n Email id is \(String(describing: email))") }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+
