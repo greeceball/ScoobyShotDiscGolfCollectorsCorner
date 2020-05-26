e1a6732a085702300cf2c6034f3d4de0225c5e2a
greeceball <griff33@gmail.com>
2020-05-26 09:53:11 -0600

    added on to the profile views

---
+++ CollectorsCorner/CollectorsCorner/Controllers/View Controllers/MyCollection/CreateDiscViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateDiscViewController.swift
+//  CollectorsCorner
+//
+//  Created by Colby Harris on 5/25/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import UIKit
+
+class CreateDiscViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CollectorsCorner/CollectorsCorner/Controllers/View Controllers/MyCollection/MyCollectionTableViewController.swift
+++ CollectorsCorner/CollectorsCorner/Controllers/View Controllers/MyCollection/MyCollectionTableViewController.swift
@@ -29,7 +29,7 @@ class MyCollectionTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "discCell", for: indexPath)
 
         // Configure the cell...
 
